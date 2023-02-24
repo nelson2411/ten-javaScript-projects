bb9802e3212b7ab5cff9db1dc2cf1ace5cea5086
Nelson Rosales <nelson.rosales83@gmail.com>
2023-02-24 10:20:02 -0600

    feat: project init, form validator

--- drag&drop/script.js
+++ drag&drop/script.js
@@ -149,7 +149,7 @@ function showInputBox(column) {
 function hideInputBox(column) {
   addBtns[column].style.visibility = "visible"
   saveItemBtns[column].style.display = "none"
-  addItemContainers[column].style.display = "flex"
+  addItemContainers[column].style.display = "none"
   addToColumn(column)
 }
 
---
+++ form-validator/script.js
@@ -0,0 +1,9 @@
+/*
+q: tell me the difference between parameters and arguments
+a: parameters are the names listed in the function 
+definition
+arguments are the real values passed to the function
+
+
+
+*/
