b2cb19246ea5941bb17ca5005ced8be98d689979
Nelson Rosales <nelson.rosales83@gmail.com>
2023-02-27 20:08:48 -0600

    javascript implemented

--- form-validator/script.js
+++ form-validator/script.js
@@ -1,9 +1,30 @@
 /*
-q: tell me the difference between parameters and arguments
-a: parameters are the names listed in the function 
-definition
-arguments are the real values passed to the function
-
-
+Form Validator
 
 */
+
+const form = document.getElementById("form")
+const password1El = document.getElementById("password1")
+const password2El = document.getElementById("password2")
+const messageContainer = document.querySelector(".message-container")
+const message = document.getElementById("message")
+
+let isValid = false
+
+const validateForm = () => {
+  // Using Constraint API
+  isValid = form.checkValidity()
+  // Style main message for an error
+  message.textContent = "Please fill out all fields."
+  message.style.color = "red"
+  messageContainer.style.borderColor = "red"
+}
+
+const processFormData = (e) => {
+  e.preventDefault()
+  // validate form
+  validateForm()
+}
+
+// Event listener
+form.addEventListener("submit", processFormData)
