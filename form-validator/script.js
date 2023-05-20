22362d6d3f629e7cb10668e7273ab8bbdebca5a8
Nelson Rosales <nelson.rosales83@gmail.com>
2023-05-20 18:02:07 +0200

    feat: project done

--- form-validator/script.js
+++ form-validator/script.js
@@ -10,20 +10,53 @@ const messageContainer = document.querySelector(".message-container")
 const message = document.getElementById("message")
 
 let isValid = false
+let passwordsMatch = false // a boolean to check if passwords match
 
 const validateForm = () => {
   // Using Constraint API
   isValid = form.checkValidity()
   // Style main message for an error
-  message.textContent = "Please fill out all fields."
-  message.style.color = "red"
-  messageContainer.style.borderColor = "red"
+  if (!isValid) {
+    message.textContent = "Please fill out all fields."
+    message.style.color = "red"
+    messageContainer.style.borderColor = "red"
+  }
+  // check if passwords match
+  if (password1El.value === password2El.value) {
+    passwordsMatch = true
+    password1El.style.borderColor = "green"
+    password2El.style.borderColor = "green"
+  } else {
+    passwordsMatch = false
+    message.textContent = "Make sure passwords match."
+    message.style.color = "red"
+    messageContainer.style.borderColor = "red"
+    password1El.style.borderColor = "red"
+    password2El.style.borderColor = "red"
+    return
+  }
+}
+
+function storeFormData() {
+  const user = {
+    name: form.name.value,
+    phone: form.phone.value,
+    email: form.email.value,
+    website: form.website.value,
+    password: form.password.value,
+  }
+  // Do something with user data
+  console.log(user)
 }
 
 const processFormData = (e) => {
   e.preventDefault()
   // validate form
   validateForm()
+  // submit data if valid
+  if (isValid && passwordsMatch) {
+    storeFormData()
+  }
 }
 
 // Event listener
