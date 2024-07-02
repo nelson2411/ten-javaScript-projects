0a515a7cd553f734b6c216189be1912fbec4d8d0
Nelson Rosales <nelson.rosales83@gmail.com>
2024-07-02 13:11:47 +0200

    chore: Add shuffle function for array randomization

--- Math_Sprint_Game/script.js
+++ Math_Sprint_Game/script.js
@@ -1,84 +1,188 @@
 // Pages
-const gamePage = document.getElementById('game-page');
-const scorePage = document.getElementById('score-page');
-const splashPage = document.getElementById('splash-page');
-const countdownPage = document.getElementById('countdown-page');
+const gamePage = document.getElementById("game-page")
+const scorePage = document.getElementById("score-page")
+const splashPage = document.getElementById("splash-page")
+const countdownPage = document.getElementById("countdown-page")
 // Splash Page
-const startForm = document.getElementById('start-form');
-const radioContainers = document.querySelectorAll('.radio-container');
-const radioInputs = document.querySelectorAll('input');
-const bestScores = document.querySelectorAll('.best-score-value');
+const startForm = document.getElementById("start-form")
+const radioContainers = document.querySelectorAll(".radio-container")
+const radioInputs = document.querySelectorAll("input")
+const bestScores = document.querySelectorAll(".best-score-value")
 // Countdown Page
-const countdown = document.querySelector('.countdown');
+const countdown = document.querySelector(".countdown")
 // Game Page
-const itemContainer = document.querySelector('.item-container');
+const itemContainer = document.querySelector(".item-container")
 // Score Page
-const finalTimeEl = document.querySelector('.final-time');
-const baseTimeEl = document.querySelector('.base-time');
-const penaltyTimeEl = document.querySelector('.penalty-time');
-const playAgainBtn = document.querySelector('.play-again');
+const finalTimeEl = document.querySelector(".final-time")
+const baseTimeEl = document.querySelector(".base-time")
+const penaltyTimeEl = document.querySelector(".penalty-time")
+const playAgainBtn = document.querySelector(".play-again")
 
 // Equations
-
-let equationsArray = [];
+let questionAmount = 0
+let equationsArray = []
+let playerGuessArray = [] // This will store the player's guesses, either true or false
 
 // Game Page
-let firstNumber = 0;
-let secondNumber = 0;
-let equationObject = {};
-const wrongFormat = [];
+let firstNumber = 0
+let secondNumber = 0
+let equationObject = {}
+const wrongFormat = []
 
 // Time
 
 // Scroll
+let valueY = 0 // This value will represent the Y axis of the scroll bar
+
+// Scroll and store the value in valueY
+const select = (guessedTrue) => {
+  console.log("player guess:", playerGuessArray)
+  // Scroll 80 pixels
+  valueY += 80
+  itemContainer.scroll(0, valueY)
+  // Add player guess to array
+  return guessedTrue
+    ? playerGuessArray.push("true")
+    : playerGuessArray.push("false")
+}
+
+// Display Game Page
+const showGamePage = () => {
+  gamePage.hidden = false
+  countdownPage.hidden = true
+}
+
+// Get Random Number up to a certain number
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * Math.floor(max))
+}
 
 // Create Correct/Incorrect Random Equations
 function createEquations() {
   // Randomly choose how many correct equations there should be
-  // const correctEquations = 
+  const correctEquations = getRandomInt(questionAmount)
   // Set amount of wrong equations
-  // const wrongEquations = 
+  const wrongEquations = questionAmount - correctEquations
+  console.log("correct equations:", correctEquations)
+  console.log("wrong equations:", wrongEquations)
   // Loop through, multiply random numbers up to 9, push to array
-  // for (let i = 0; i < correctEquations; i++) {
-  //   firstNumber = 
-  //   secondNumber = 
-  //   const equationValue = firstNumber * secondNumber;
-  //   const equation = `${firstNumber} x ${secondNumber} = ${equationValue}`;
-  //   equationObject = { value: equation, evaluated: 'true' };
-  //   equationsArray.push(equationObject);
-  // }
+  for (let i = 0; i < correctEquations; i++) {
+    firstNumber = getRandomInt(9) // the max is 9 because we want to multiply by 9
+    secondNumber = getRandomInt(9) // the max is 9 because we want to multiply by 9
+    const equationValue = firstNumber * secondNumber
+    const equation = `${firstNumber} x ${secondNumber} = ${equationValue}`
+    equationObject = { value: equation, evaluated: "true" }
+    equationsArray.push(equationObject)
+  }
   // Loop through, mess with the equation results, push to array
-  // for (let i = 0; i < wrongEquations; i++) {
-  //   firstNumber = 
-  //   secondNumber = 
-  //   const equationValue = firstNumber * secondNumber;
-  //   wrongFormat[0] = `${firstNumber} x ${secondNumber + 1} = ${equationValue}`;
-  //   wrongFormat[1] = `${firstNumber} x ${secondNumber} = ${equationValue - 1}`;
-  //   wrongFormat[2] = `${firstNumber + 1} x ${secondNumber} = ${equationValue}`;
-  //   const formatChoice = 
-  //   const equation = wrongFormat[formatChoice];
-  //   equationObject = { value: equation, evaluated: 'false' };
-  //   equationsArray.push(equationObject);
-  // }
+  for (let i = 0; i < wrongEquations; i++) {
+    firstNumber = getRandomInt(9) // the max is 9 because we want to multiply by 9
+    secondNumber = getRandomInt(9) // the max is 9 because we want to multiply by 9
+    const equationValue = firstNumber * secondNumber
+    wrongFormat[0] = `${firstNumber} x ${secondNumber + 1} = ${equationValue}`
+    wrongFormat[1] = `${firstNumber} x ${secondNumber} = ${equationValue - 1}`
+    wrongFormat[2] = `${firstNumber + 1} x ${secondNumber} = ${equationValue}`
+    const formatChoice = getRandomInt(3)
+    const equation = wrongFormat[formatChoice]
+    equationObject = { value: equation, evaluated: "false" }
+    equationsArray.push(equationObject)
+  }
+  shuffle(equationsArray)
+}
+
+// Add equations to DOM
+function equationsToDOM() {
+  equationsArray.forEach((equation) => {
+    // Item
+    const item = document.createElement("div")
+    item.classList.add("item")
+    // Equation Text
+    const equationText = document.createElement("h1")
+    equationText.textContent = equation.value
+    // Append
+    item.appendChild(equationText)
+    itemContainer.appendChild(item)
+  })
 }
 
 // Dynamically adding correct/incorrect equations
-// function populateGamePage() {
-//   // Reset DOM, Set Blank Space Above
-//   itemContainer.textContent = '';
-//   // Spacer
-//   const topSpacer = document.createElement('div');
-//   topSpacer.classList.add('height-240');
-//   // Selected Item
-//   const selectedItem = document.createElement('div');
-//   selectedItem.classList.add('selected-item');
-//   // Append
-//   itemContainer.append(topSpacer, selectedItem);
+function populateGamePage() {
+  // Reset DOM, Set Blank Space Above
+  itemContainer.textContent = ""
+  // Spacer
+  const topSpacer = document.createElement("div")
+  topSpacer.classList.add("height-240")
+  // Selected Item
+  const selectedItem = document.createElement("div")
+  selectedItem.classList.add("selected-item")
+  // Append
+  itemContainer.append(topSpacer, selectedItem)
 
-//   // Create Equations, Build Elements in DOM
+  // Create Equations, Build Elements in DOM
+  createEquations()
+  equationsToDOM()
 
-//   // Set Blank Space Below
-//   const bottomSpacer = document.createElement('div');
-//   bottomSpacer.classList.add('height-500');
-//   itemContainer.appendChild(bottomSpacer);
-// }
+  // Set Blank Space Below
+  const bottomSpacer = document.createElement("div")
+  bottomSpacer.classList.add("height-500")
+  itemContainer.appendChild(bottomSpacer)
+}
+
+// Displays 3, 2, 1, GO!
+const countdownStart = () => {
+  countdown.textContent = "3"
+  setTimeout(() => {
+    countdown.textContent = "2"
+  }, 1000)
+  setTimeout(() => {
+    countdown.textContent = "1"
+  }, 2000)
+  setTimeout(() => {
+    countdown.textContent = "GO!"
+  }, 3000)
+}
+
+// Navigate from Splash Page to Countdown Page
+const showCountdown = () => {
+  countdownPage.hidden = false
+  splashPage.hidden = true
+  //populateGamePage()
+  countdownStart()
+  populateGamePage()
+  setTimeout(showGamePage, 400)
+}
+
+// Get the value from selected radio button
+function getRadioValue() {
+  let radioValue
+  radioInputs.forEach((radioInput) => {
+    if (radioInput.checked) {
+      radioValue = radioInput.value
+    }
+  })
+  return radioValue
+}
+
+// Form that decides amount of questions
+function selectQuestionAmount(e) {
+  e.preventDefault()
+  questionAmount = getRadioValue()
+  console.log("question amount:", questionAmount)
+  if (questionAmount) {
+    showCountdown()
+  }
+}
+
+startForm.addEventListener("click", () => {
+  radioContainers.forEach((radioEl) => {
+    // Remove Selected Label Styling
+    radioEl.classList.remove("selected-label")
+    // Add it back if radio input is checked
+    if (radioEl.children[1].checked) {
+      radioEl.classList.add("selected-label")
+    }
+  })
+})
+
+// Event Listeners ✨
+startForm.addEventListener("submit", selectQuestionAmount)
